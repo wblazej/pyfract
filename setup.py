a1701a5e2b41630c39fe7d16b763910c045ea410
wblazej <blazej.wrzosok@gmail.com>
2021-12-21 16:13:01 +0100

    removed testing file and upgraded version in setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_file(path, encoding='utf-8'):
 setup(
     name='pyfract',
     packages=find_packages(),
-    version='1.1',
+    version='1.2',
     description='Fractions implementation for Python',
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
