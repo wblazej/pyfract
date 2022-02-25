34cac6c9c520fe44aedeee4414c1d314ab17d8ac
wblazej <blazej.wrzosok@gmail.com>
2022-02-25 22:39:22 +0100

    running tests script update

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RM_PATH=$(which rm)
-PYTHON3_PATH=$(which python3)
-PATH=$(pwd)
-$RM_PATH -rf $PATH/dist/ $PATH/build/
-$PYTHON3_PATH setup.py bdist_wheel
-$PYTHON3_PATH -m pip install dist/pyfract-*.whl --force-reinstall
-$PYTHON3_PATH $PATH/tests/tests.py
+rm=$(which rm)
+python3=$(which python3)
+$rm -rf $PWD/dist/ $PWD/build/
+$python3 setup.py bdist_wheel
+$python3 -m pip install dist/pyfract-*.whl --force-reinstall
+$rm -rf $PWD/dist/ $PWD/build/
+$python3 $PWD/tests/tests.py
